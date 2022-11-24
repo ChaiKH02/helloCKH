a8eff12e9b11d2a99fb6f9e50c2e8a821dc9ccf2
anreh <anurehka@gmail.com>
2022-11-24 13:59:22 +0800

    Initial commit

---
+++ app/src/main/java/my/edu/tarc/hellockh/MainActivity.kt
@@ -0,0 +1,11 @@
+package my.edu.tarc.hellockh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main) //create the ui
+    }
+}
